431691e92b3520a137ef7a067afffde245a21c91
Sayak Ghorai <sayakghorai@Sayaks-MacBook-Air-M1.local>
2024-03-30 23:18:08 +0530

    Update Author module

--- frontend/src/components/update/UpdateAuthor.js
+++ frontend/src/components/update/UpdateAuthor.js
@@ -1,58 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import SearchBar from '../search_comp/SearchBar';
+import ItemList from '../search_comp/ItemList';
 
-const SearchBar = ({ onSearch }) => {
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const handleSearch = () => {
-    onSearch(searchQuery);
-  };
-
-  return (
-    <div className="flex justify-center mb-4">
-      <input
-        type="text"
-        placeholder="Search author..."
-        className="border bg-gray-200 border-gray-300 rounded-lg p-2 w-64 mr-2"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <button
-        type="button"
-        className="bg-blue-700 text-white py-2 px-4 rounded font-semibold hover:bg-blue-600 focus:ring-4 focus:ring-blue-500"
-        onClick={handleSearch}
-      >
-        Search
-      </button>
-    </div>
-  );
-};
-
-const AuthorList = ({ authors, onSelectAuthor }) => {
-  return (
-    <div className="mb-4">
-      {
-        authors.map((author) => (
-          <div
-            key={author._id}
-            className="bg-gray-700 p-2 rounded-lg mb-2 cursor-pointer"
-            onClick={() => onSelectAuthor(author)}
-          >
-            {author.authorName} - {author.authorEmail && author.authorEmail}
-          </div>
-        ))
-        }
-    </div>
-  );};
-
-
-
-const UpdateBorrower = () => {
+const UpdateAuthor = () => {
+  const [showAuthorList, setShowAuthorList] = useState(true);
   const [formData, setFormData] = useState({
     authorName: '',
     authorEmail: ''
   });
 
-  const [authors, setAuthors] = useState([]); // Changed from setUsers to setBorrowers
+  const [authors, setAuthors] = useState([]);
 
   const [selectedAuthor, setSelectedAuthor] = useState(null);
 
@@ -61,11 +18,11 @@ const UpdateBorrower = () => {
         console.log('Query:', query);
         const response = await fetch(`${process.env.REACT_APP_API_URI}/authors/search?query=${query}`);
         const data = await response.json();
-        setAuthors(data); // Changed from setUsers to setBorrowers
+        setAuthors(data);
     } 
     catch (error) {
         console.error('Error fetching authors:', error);
-        setAuthors([]); // Ensure authors is set to an empty array in case of error
+        setAuthors([]);
     }
   };
 
@@ -109,11 +66,22 @@ const UpdateBorrower = () => {
             authorName: '',
             authorEmail: ''
         });
-        setAuthors([]); // Changed from setUsers to setBorrowers
+        setAuthors([]);
+        setSelectedAuthor(null);
       }
     } catch (error) {
       console.error('Error updating author:', error);
     }
+  };
+
+  const handleReset = () => {
+    setFormData({
+      authotName: '',
+      authorEmail: ''
+    });
+    setSelectedAuthor(null);
+    setAuthors([]);
+    setShowAuthorList(true);
   };
 
   useEffect(() => {
@@ -128,45 +96,55 @@ const UpdateBorrower = () => {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold text-center mb-6">Update Author</h1>
+      
+      
+      {!selectedAuthor? (
       <div className='text-gray-500'>
-        <SearchBar onSearch={handleSearch} />
-        <AuthorList authors={authors} onSelectAuthor={handleSelectAuthor} /> 
+        <SearchBar onSearch={handleSearch} placeholder="Search Authors..."/>
+        <ItemList items={authors} onSelectItem={handleSelectAuthor} isVisible={showAuthorList} itemType="author" /> 
+      </div>): (
+      <>
+        <form onSubmit={handleSubmit} className='w-screen max-w-md'>
+          <div className="bg-gray-700 rounded-lg p-4 mt-4">
+            <div className="mb-4">
+              <label htmlFor="author_name" className="block">Name</label>
+              <input
+                type="text"
+                name="authorName"
+                id="author_name"
+                placeholder="Authors's Name"
+                className="border  bg-gray-200 text-gray-500 border-gray-300 rounded-lg p-2 w-full"
+                value={formData.authorName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="author_email" className="block">Email</label>
+              <input
+                type="email"
+                name="authorEmail"
+                id="author_email"
+                placeholder="Author's Email"
+                className="border bg-gray-200 text-gray-500 border-gray-300 rounded-lg p-2 w-full"
+                value={formData.authorEmail}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="mt-4">
+            <button type="submit" className="bg-blue-700 text-white py-4 w-full rounded font-semibold hover:bg-blue-600 focus:ring-4 focus:ring-blue-500">
+              Update Author
+            </button>
+          </div>
+        </form>
+        <div className="mt-2">
+        <button onClick={handleReset} className="bg-red-100 text-red-500 py-4 mb-4 w-full rounded font-semibold hover:bg-red-200 ring-4 ring-red-300 focus:ring-4 focus:ring-red-500">
+          Clear
+        </button>
       </div>
-      {selectedAuthor && (<form onSubmit={handleSubmit} className='w-screen max-w-md'>
-        <div className="bg-gray-700 rounded-lg p-4 mt-4">
-          <div className="mb-4">
-            <label htmlFor="author_name" className="block">Name</label>
-            <input
-              type="text"
-              name="authorName"
-              id="author_name"
-              placeholder="Authors's Name"
-              className="border  bg-gray-200 text-gray-500 border-gray-300 rounded-lg p-2 w-full"
-              value={formData.authorName}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="author_email" className="block">Email</label>
-            <input
-              type="email"
-              name="authorEmail"
-              id="author_email"
-              placeholder="Author's Email"
-              className="border bg-gray-200 text-gray-500 border-gray-300 rounded-lg p-2 w-full"
-              value={formData.authorEmail}
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-        <div className="mt-4">
-          <button type="submit" className="bg-blue-700 text-white py-4 w-full rounded font-semibold hover:bg-blue-600 focus:ring-4 focus:ring-blue-500">
-            Update Author
-          </button>
-        </div>
-      </form>)}
+      </>)}
     </div>
   );
 };
 
-export default UpdateBorrower;
+export default UpdateAuthor;
